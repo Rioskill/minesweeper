d6499660284de88046ff035594c26ae4e7e9c91a
nikstarling <rioskill@yandex.ru>
2024-07-29 21:27:28 +0300

    added comment inside fragment shader

--- src/shaders/fragmentShader.fs
+++ src/shaders/fragmentShader.fs
@@ -17,12 +17,19 @@ varying vec2 globalCoords;
 varying highp vec2 vTextureCoord;
 
 float width = 1. / 11.;
+float stroke = 2.;
+
+vec3 gridBorderColor = vec3(0.7);
+vec3 scrollbarColor = vec3(0., 1., 0);
 
 #define FLAG 1
 #define MINE 2
 #define CLOSED 3
 
 int getType(vec2 orig) {
+    // closed = +100
+    // flag = +50
+
     if (orig.x >= 150. * width) {
         return FLAG;
     }
@@ -35,14 +42,14 @@ int getType(vec2 orig) {
         return CLOSED;
     }
 
-    if (orig.x >= 50. * width) {
-        return FLAG;
-    }
-
     return 0;
 }
 
 float getTexX(int tileType, float tileX, float origX) {
+    // FLAG is 9-nth
+    // MINE is 10-nth
+    // CLOSED is 1-st
+
     if (tileType == FLAG) {
         return (tileX + 9.) * width;
     } else if (tileType == MINE) {
@@ -61,6 +68,9 @@ vec2 getTexCoord(vec2 orig, float tileX) {
 }
 
 bool isClosed(float coordX) {
+    // CLOSED = +100
+    // MINE = 10
+
     return coordX >= 100. * width &&
             coordX <  110. * width ||
             coordX >= 110. * width &&
@@ -91,8 +101,6 @@ vec4 blendColors(vec3 first, vec3 second, float a) {
 
 void main()
 {   
-    // vec2 st = (gl_FragCoord.xy) / fullSize;
-
     vec4 closedColor = vec4(0.8, 0.77, 0.7, 1.);
 
     vec2 p = fract(pos) * fullSize / matrixSize;
@@ -101,11 +109,9 @@ void main()
 
     float pXPart = p.x / maxP.x;
 
-    float stroke = 2.;
-
     if (isScrollBar())
     {
-        gl_FragColor = vec4(vec3(0., 1., 0.), 1.);
+        gl_FragColor = vec4(scrollbarColor, 1.);
         return;
     }
 
@@ -114,43 +120,25 @@ void main()
     if (isBorder(p, maxP, stroke)) {
 
         if (isClosed(vTextureCoord.x) || tileType == FLAG) {
+            // make shadows for 3d illusion
+            // top left is white
             if (p.x < stroke || p.y > maxP.x - stroke) {
                 gl_FragColor = vec4(1.);
+            // bottom right is black
             } else {
                 gl_FragColor = vec4(vec3(0.), 1.);
             }
         } else {
-            gl_FragColor = vec4(vec3(0.7), 1.);
+            gl_FragColor = vec4(gridBorderColor, 1.);
         } 
 
     } else {
         vec2 texCoord = getTexCoord(vTextureCoord, pXPart);
 
         if (isClosed(vTextureCoord.x)) {
-            // gl_FragColor = vec4(0.8, 0.77, 0.7, 1.);
             gl_FragColor = closedColor;
         } else {
-
             vec4 c = texture2D(uSampler, texCoord);
-
-            // if (c.a < 0.) {
-            //     gl_FragColor = vec4(0., 0., 1., 1.);
-            // } else {
-            //     gl_FragColor = c; 
-            // }
-
-            // gl_FragColor = texture2D(uSampler, texCoord);
-
-            // gl_FragColor = vec4(c.rgb * c.a + closedColor.rgb * (1. - c.a), 1.);
-
-            // vec3 secondColor;
-
-            // if (tileType == FLAG) {
-            //     secondColor = closedColor.rgb;
-            // } else if (tileType == MINE) {
-            //     secondColor = vec3(1., 0., 0.);
-            // }
-
             gl_FragColor = blendColors(c.rgb, closedColor.rgb, c.a);
         }
     }
